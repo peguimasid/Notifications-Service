d68a925e5939c2b90dba256e75511c76f87c454d
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-13 15:30:03 -0300

    list and create notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,26 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Notification } from '@prisma/client';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'crypto';
 
 @Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  async countNotifications(): Promise<number> {
-    return await this.prisma.notification.count();
+  async list(): Promise<Notification[]> {
+    return await this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(): Promise<Notification> {
+    return await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content: 'You are selected',
+        category: 'promotion',
+        recipientId: '7899-9293-2737',
+      },
+    });
   }
 }
