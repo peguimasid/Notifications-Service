1200060d18dfe115cc6dfa628eb4967bfd394027
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-14 20:00:50 -0300

    in memory database

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,8 +1,11 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository';
 import { SendNotification } from './send-notification';
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const notificationsRepository = new InMemoryNotificationsRepository();
+
+    const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
       content: 'Você recebeu uma solicitação de amizade',
@@ -10,6 +13,7 @@ describe('Send notification', () => {
       recipientId: 'recipient-id-example',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
