086090bf8a6ca564c56f61c170589667be91b2b2
Guilhermo Masid <guilhermomasid@gmail.com>
2022-12-16 15:01:32 -0300

    improve folders architechture

---
+++ src/infra/database/prisma/repositories/prisma-notifications-repository.ts
@@ -0,0 +1,20 @@
+import { Notification } from 'src/application/entities/notification';
+import { NotificationRepository } from '../../../../application/repositories/notifications-repository';
+import { PrismaService } from '../prisma.service';
+
+export class PrismaNotificationsRepository implements NotificationRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(notification: Notification): Promise<void> {
+    await this.prismaService.notification.create({
+      data: {
+        id: notification.id,
+        category: notification.category,
+        content: notification.content.value,
+        recipientId: notification.recipientId,
+        readAt: notification.readAt,
+        createdAt: notification.createdAt,
+      },
+    });
+  }
+}
